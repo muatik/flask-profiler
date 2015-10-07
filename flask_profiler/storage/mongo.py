6a8899abb48f4bb82204466e398a28088696cb60
fatihsucu <fatihsucu0@gmail.com>
2015-10-07 15:16:11 +0300

    fixes bugs.

--- flask_profiler/storage/mongo.py
+++ flask_profiler/storage/mongo.py
@@ -2,12 +2,12 @@ import pymongo
 from base import BaseStorage
 import datetime
 from bson.objectid import ObjectId
-
+import random
 
 class Mongo(BaseStorage):
     """docstring for Mongo"""
 
-    def __init__(self, config):
+    def __init__(self, config=None):
         super(Mongo, self).__init__(),
         self.host = "localhost"
         self.database_name = "profiler"
@@ -27,20 +27,25 @@ class Mongo(BaseStorage):
 
     def filter(self, kwargs):
         query = {}
-
+        limit = kwargs.get('limit', 100000)
+        skip = kwargs.get('skip', 0)
+        sort_dir = kwargs.get('sort', "asc")
+        sort_key = kwargs.get('sort_by', "_id")
         endedAt = kwargs.get('endedAt', None)
         startedAt = kwargs.get('startedAt', None)
         elapsed = kwargs.get('elapsed', None)
         name = kwargs.get('name', None)
         method = kwargs.get('method', None)
-        url = kwargs.get('url', None)
+
+        if sort_dir == "desc":
+            sort_dir = pymongo.DESCENDING
+        else:
+            sort_dir = pymongo.ASCENDING
 
         if name:
             query['name'] = name
         if method:
             query['method'] = method
-        if url:
-            query['url'] = url
         if endedAt:
             query['endedAt'] = { "$lte": endedAt }
         if startedAt:
@@ -56,39 +61,36 @@ class Mongo(BaseStorage):
             cursor = self.collection.find(
                 query
                 ).sort(sort_key, sort_dir).skip(skip).limit(limit)
-        return self.clearify(cursor)
+        return (self.clearify(record) for record in cursor)
 
     def insert(self, recordDictionary):
         result = self.collection.insert(recordDictionary)
-        if result["n"] > 0:
+        if result:
             return True
         return False
 
     def delete(self, measurementId):
         result = self.collection.remove({"_id": ObjectId(measurementId)})
-        if result["n"] > 0:
+        if result:
             return True
         return False
 
     def getSummary(self,  kwargs):
         match_condition = {}
-        endDate = kwargs.get('endedAt', None)
+        endedAt = kwargs.get('endedAt', None)
         startedAt = kwargs.get('startedAt', None)
         elapsed = kwargs.get('elapsed', None)
         name = kwargs.get('name', None)
         method = kwargs.get('method', None)
-        url = kwargs.get('url', None)
 
         if name:
             match_condition['name'] = name
         if method:
             match_condition['method'] = method
-        if url:
-            match_condition['url'] = url
-        if endDate:
-            match_condition['endedAt'] = { "$lte": endDate }
-        if startDate:
-            match_condition['startedAt'] = { "$gt": startDate }
+        if endedAt:
+            match_condition['endedAt'] = { "$lte": endedAt }
+        if startedAt:
+            match_condition['startedAt'] = { "$gt": startedAt }
         if elapsed:
             match_condition['elapsed'] = { "$gte": elapsed }
 
@@ -97,8 +99,7 @@ class Mongo(BaseStorage):
                     {"$group": {
                         "_id": {
                             "method": "$method",
-                            "name": "$name",
-                            "url": "$context.url"
+                            "name": "$name"
                             },
                         "count": { "$sum": 1},
                         "min": {"$min": "$elapsed" },
@@ -109,16 +110,16 @@ class Mongo(BaseStorage):
                 )
         return result
 
-    def clearify(self, cursor):
+    def clearify(self, obj):
         available_types = [int, dict, str]
-        for i in cursor:
-            for k, v in i:
-                if any([isinstance(v, av_type) for av_type in available_types]):
-                    continue
-                if k == "_id":
-                    k = "id"
-                i[k] = str(v)
-            yield i
+        for k, v in obj.items():
+            if any([isinstance(v, av_type) for av_type in available_types]):
+                continue
+            if k == "_id":
+                k = "id"
+                obj.pop("_id")
+            obj[k] = str(v)
+        return obj
 
     def get(self, measurementId):
         record = self.collection.find_one({'_id': ObjectId(measurementId)})
