99f5754f7e05f5c5f7b7b4c37986b783fb9d1795
Vojta Bartos <me@vojtechbartos.cz>
2016-05-03 10:55:33 +0200

    Added ability to be able use custom engine
    - engine specified as string module and class path `foo.bar.example.RedisStorage`

--- flask_profiler/storage/__init__.py
+++ flask_profiler/storage/__init__.py
@@ -1,15 +1,34 @@
 # -*- coding: utf8 -*-
 
+import importlib
+from .base import BaseStorage
+
 
 def getCollection(conf):
-    engine = conf.get("engine", "").lower()
-    if engine == "mongodb":
+    engine = conf.get("engine", "")
+    if engine.lower() == "mongodb":
         from .mongo import Mongo
         return Mongo(conf)
-    elif engine == "sqlite":
+    elif engine.lower() == "sqlite":
         from .sqlite import Sqlite
         return Sqlite(conf)
     else:
-        raise ValueError(
-            ("flask-profiler requires a valid storage engine but it is missing"
-                " or wrong. provided engine: {}".format(engine)))
+        try:
+            engine = engine.split('.')
+            if len(engine) < 1: # engine must have at least module name and class
+                raise ImportError
+
+            module_name = '.'.join(engine[:-1])
+            klass_name = engine[-1]
+            module = importlib.import_module(module_name)
+            storage = getattr(module, klass_name)
+            if not issubclass(storage, BaseStorage):
+                raise ImportError
+
+        except ImportError:
+            raise ValueError(
+                ("flask-profiler requires a valid storage engine but it is"
+                    " missing or wrong. provided engine: {}".format(engine)))
+
+
+        return storage(conf)
