b96e67410196994e26ed210bcee2c1f71e7ac309
Anand Tripathi <anand.tripathi507@gmail.com>
2018-02-09 14:56:27 +0530

    Update basetest.py

--- tests/basetest.py
+++ tests/basetest.py
@@ -36,7 +36,7 @@ _CONFS = {
         "enabled": True,
         "storage": {
             "engine": "sqlalchemy",
-            "db_url": "sqlite:///flask_profiler.sql"
+            "db_url": "mysql://root:@localhost/flask_profiler"
         },
         "ignore": [
             "^/static/.*"
