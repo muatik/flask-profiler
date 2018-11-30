23aa8699867fb01a12ddb7ceed17717675e5876b
Oldřich Taufer <O.Taufer@gmail.com>
2018-11-30 14:11:18 +0100

    add jsonrpc support

--- tests/test_measure_endpoint.py
+++ tests/test_measure_endpoint.py
@@ -3,7 +3,7 @@ import unittest
 
 from flask_testing import TestCase as FlaskTestCase
 
-from .basetest import BasetTest, BaseTest2, flask_profiler
+from .basetest import BasetTest, BaseTest2, flask_profiler, JsonrpcBaseTest
 
 
 class EndpointMeasurementTest(BasetTest, FlaskTestCase):
@@ -67,6 +67,29 @@ class EndpointMeasurementTest2(BaseTest2, FlaskTestCase):
                 self.assertEqual(list_element["kwargs"], {"message": "hello"})
                 self.assertEqual(list_element["context"]["args"], {"q": "2"})
         self.assertEqual(True, test_flag)
+
+
+class JsonrpcMeasurementTest(JsonrpcBaseTest, FlaskTestCase):
+
+    def test_01_profiler(self):
+        self.client.post("/v1", data={"jsonrpc": "2.0", "method": "people.index", "id": "1"})
+
+        measurements = list(flask_profiler.collection.filter())
+
+        self.assertEqual(len(measurements), 1)
+        self.assertEqual(measurements[0]['name'], 'people.index')
+
+    def test_02_profiler(self):
+        self.client.post("/v1", data={"jsonrpc": "2.0", "method": "people.index", "id": "1"})
+        self.client.post("/v1", data={"jsonrpc": "2.0", "method": "people.index", "id": "1"})
+        self.client.post("/v1", data={"jsonrpc": "2.0", "method": "people.get", "id": "1"})
+
+        measurements = list(flask_profiler.collection.filter())
+
+        self.assertEqual(len(measurements), 3)
+        self.assertEqual(measurements[0]['name'], 'people.index')
+        self.assertEqual(measurements[1]['name'], 'people.index')
+        self.assertEqual(measurements[2]['name'], 'people.get')
 
 
 if __name__ == '__main__':
