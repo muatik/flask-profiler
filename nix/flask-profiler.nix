b0f70aeccc5ac0abbf04a7c513b6e628b69fe8d4
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-11-19 12:26:08 +0100

    Remove setup.py in favour of pyproject.toml

--- nix/flask-profiler.nix
+++ nix/flask-profiler.nix
@@ -1,9 +1,11 @@
 { buildPythonPackage, simplejson, flask-httpauth, flask
-, sqlalchemy, flask-testing, pytestCheckHook }:
+, sqlalchemy, flask-testing, pytestCheckHook, setuptools }:
 buildPythonPackage rec {
   pname = "flask_profiler";
   version = "master";
   src = ../.;
+  buildInputs = [ setuptools ];
   propagatedBuildInputs = [ simplejson flask-httpauth flask ];
   checkInputs = [ sqlalchemy flask-testing pytestCheckHook ];
+  format = "pyproject";
 }
